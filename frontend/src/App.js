785be79d077c35dac3a83f5d589758dd7288378a
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-11-24 11:34:04 +1100

    Silent Auth working. OAuth Google working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Questions from './components/Questions/Questions';
 import Question from './components/Question/Question';
 import Callback from './components/Callback/Callback';
 import NewQuestion from './components/NewQuestion/NewQuestion';
 import SecuredRoute from './components/SecuredRoute/SecuredRoute';
+import auth0Client from './Auth';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({checkingSession:false});
+      return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({checkingSession:false});
+  }
+
   render() {
     return (
       <div>
@@ -16,10 +38,12 @@ class App extends Component {
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
         <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-question' component={NewQuestion} />
+        <SecuredRoute path='/new-question'
+        component={NewQuestion}
+        checkingSession={this.state.checkingSession} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
