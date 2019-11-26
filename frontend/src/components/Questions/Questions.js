e3c7ea6f887cc2ea916510ede2d74de31b54e50e
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-11-26 20:10:17 +1100

    minor changes

--- frontend/src/components/Questions/Questions.js
+++ frontend/src/components/Questions/Questions.js
@@ -38,7 +38,7 @@ class Questions extends Component {
               <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
                 <Link to={`/question/${question._id}`}>
                   <div className="card text-white bg-success mb-3">
-                    <div className="card-header">Answers: {question.answers}</div>
+                    <div className="card-header">Answers: {question.answers.length}</div>
                     <div className="card-body">
                       <h4 className="card-title">{question.title}</h4>
                       <p className="card-text">{question.description}</p>
